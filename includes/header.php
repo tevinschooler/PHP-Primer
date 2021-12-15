c0ea67d5e814ebe31785a840de0041a3a64e1561
Tevin Schooler <60293518+tevinschooler@users.noreply.github.com>
2021-12-15 15:33:44 -0500

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" 
+    crossorigin="anonymous">
+    
+ 
+</head>
+<body>
+<div class="container">
+
+
+<h2> <center>  Follow each link to the page example </center></h2>
+<ul class="nav nav-tabs">
+<li class="nav-item"><a class="nav-link" href="Index.php"> Introduction to PHP </a></li>
+    <li class="nav-item"><a class="nav-link" href="array.php"> Simple of Array and Prinouts </a></li>
+    <li class="nav-item"><a class="nav-link" href="forloop.php"> Simple of for loop </a></li>
+    <li class="nav-item"><a class="nav-link" href="dowhileloop.php"> Simple of A Do While loop</a></li>
+    <li class="nav-item"><a class="nav-link" href="ifstatement.php"> Simple of A if statement </a></li>
+    <li class="nav-item"><a class="nav-link" href="switchstatement.php"> Simple of A Switch Statement </a></li>
+    <li class="nav-item"><a class="nav-link" href="stringmani.php"> Simple of A String Manipulation </a></li>
+    <li class="nav-item"><a class="nav-link" href="dateandtime.php"> Simple of Date and time </a></li>
+    <li class="nav-item"><a class="nav-link" href="functions.php"> Simple of a functions </a></li>
+
+
+    
+
+</ul>
+
